f677e90adb85d14d2a91df6de19e831c1282b69e
Fedorenko Dmitriy <d.fedorenko@probkiizokna.ru>
2015-05-19 13:24:58 +0300

    Init.

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"net/http"
+	logging "github.com/op/go-logging"
+)
+
+var logger = logging.MustGetLogger("SERVER")
+
+const listen = "localhost:40000"
+
+func main() {
+	serveMux := http.NewServeMux()
+
+	fileSystem := http.Dir("public")
+	serveMux.Handle("/", http.FileServer(fileSystem))
+
+	logger.Info("Start listen on %s", listen)
+	if err := http.ListenAndServe(listen, serveMux); nil != err {
+		logger.Error(err.Error())
+	}
+}
